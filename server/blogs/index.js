e392fdd084909958870c5e3aa90df00e575a0675
Vitaly Puzrin <vitaly@rcdesign.ru>
2012-08-08 19:21:51 +0400

    added parameters schemas

--- server/blogs/index.js
+++ server/blogs/index.js
@@ -2,6 +2,14 @@
 
 /*global nodeca*/
 
+
+// Validate input parameters
+//
+var params_schema = {
+}
+nodeca.validate(params_schema);
+
+
 module.exports = function (params, next) {
   this.response.data.req_time = Date.now();
   this.response.layout = 'default.blogs';
