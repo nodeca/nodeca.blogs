2cc448bd2a6ae6afab2c30d14a8d1f3fddea1311
Alex Kocharin <alex@kocharin.ru>
2018-04-10 16:27:15 +0000

    Add prefetch in tracker

--- internal/users/tracker/fetch/blog_entry.js
+++ internal/users/tracker/fetch/blog_entry.js
@@ -1,5 +1,21 @@
-// Fetch entries for tracker
+// Fetch blog entries for tracker
 //
+// In:
+//
+//  - params.user_info
+//  - params.subscriptions
+//  - params.start (optional) - last last_ts from previous page
+//  - params.limit - max number of entries, 0 means return count only
+//
+// Out:
+//  - count
+//  - items - { type, last_ts, id }
+//  - next  - last last_ts (contents of params.start for the next page),
+//            null if last page
+//  - users - merged with env.data.users
+//  - res   - misc data (specific to template, merged with env.res)
+//
+
 'use strict';
 
 
@@ -16,6 +32,15 @@ module.exports = function (N, apiPath) {
     let entry_subs = _.filter(locals.params.subscriptions, { to_type: N.shared.content_type.BLOG_ENTRY });
     let blog_subs = _.filter(locals.params.subscriptions, { to_type: N.shared.content_type.BLOG_SOLE });
 
+    let content_read_marks_expire = await N.settings.get('content_read_marks_expire');
+    let min_cut = new Date(Date.now() - (content_read_marks_expire * 24 * 60 * 60 * 1000));
+
+    let can_see_hellbanned = await N.settings.get('can_see_hellbanned', {
+      user_id: locals.params.user_info.user_id,
+      usergroup_ids: locals.params.user_info.usergroups
+    }, {});
+
+    let cache = locals.params.user_info.hb || can_see_hellbanned ? 'cache_hb' : 'cache';
 
     // Fetch entries by entry subscriptions
     //
@@ -24,6 +49,7 @@ module.exports = function (N, apiPath) {
     if (entry_subs.length !== 0) {
       entries = await N.models.blogs.BlogEntry.find()
                           .where('_id').in(_.map(entry_subs, 'to'))
+                          .where(cache + '.last_ts').gt(min_cut)
                           .lean(true);
     }
 
@@ -43,17 +69,13 @@ module.exports = function (N, apiPath) {
     }
 
 
-    // Sanitize entries, replace cache with cache_hb if needed
-    entries = await sanitize_entry(N, entries, locals.params.user_info);
-
-
     // Fetch read marks
     //
     let data = entries.map(entry => ({
       categoryId: entry.user,
       contentId: entry._id,
-      lastPostNumber: entry.cache.last_comment_hid,
-      lastPostTs: entry.cache.last_ts
+      lastPostNumber: entry[cache].last_comment_hid,
+      lastPostTs: entry[cache].last_ts
     }));
 
     let read_marks = await N.models.users.Marker.info(locals.params.user_info.user_id, data);
@@ -75,27 +97,81 @@ module.exports = function (N, apiPath) {
     entries = entries.filter((__, idx) => access_env.data.access_read[idx]);
 
 
-    // Collect user ids
+    // Remove entries created by ignored users (except for subscribed ones)
     //
-    locals.users = locals.users || [];
-    locals.users = locals.users.concat(_.map(entries, 'user'));
-    locals.users = locals.users.concat(_.map(entries, 'cache.last_user').filter(Boolean));
+    let entry_subs_by_id = _.keyBy(entry_subs, 'to');
 
+    let first_users = entries.map(entry => entry.user).filter(Boolean);
 
-    locals.res.blog_entries = _.keyBy(entries, '_id');
-    locals.res.read_marks = _.assign(locals.res.read_marks || {}, read_marks);
+    let ignored = _.keyBy(
+      await N.models.users.Ignore.find()
+                .where('from').equals(locals.params.user_info.user_id)
+                .where('to').in(first_users)
+                .select('from to -_id')
+                .lean(true),
+      'to'
+    );
+
+    entries = entries.filter(entry => {
+      // Topic starter is ignored, and topic is not subscribed to
+      if (ignored[entry.user] && !entry_subs_by_id[entry._id]) {
+        return false;
+      }
+
+      // Last poster is ignored, and there is only one unread message
+      // (topic still shows up if ignored user leaves multiple messages)
+      if (ignored[_.get(entry, cache + '.last_user')] &&
+          read_marks[entry._id].position >= _.get(entry, cache + '.last_comment_hid') - 1) {
+
+        return false;
+      }
+
+      return true;
+    });
+
 
     let items = [];
 
     entries.forEach(entry => {
       items.push({
         type: 'blog_entry',
-        last_ts: entry.cache.last_ts || entry.ts,
-        id: entry._id
+        last_ts: entry[cache].last_ts || entry.ts,
+        id: String(entry._id)
       });
     });
 
-    locals.res.items = _.orderBy(items, 'last_ts', 'desc');
-    locals.count = locals.res.items.length;
+    locals.count = items.length;
+
+    if (locals.params.limit > 0) {
+      if (locals.params.start) items = items.filter(item => item.last_ts.valueOf() < locals.params.start);
+
+      let items_sorted = _.orderBy(items, 'last_ts', 'desc');
+      let items_on_page = items_sorted.slice(0, locals.params.limit);
+
+      locals.items = items_on_page;
+      locals.next = items_sorted.length > items_on_page.length ?
+                    items_on_page[items_on_page.length - 1].last_ts.valueOf() :
+                    null;
+
+      // Filter only topics that are on this page
+      //
+      let entry_ids = new Set();
+      for (let { id } of items_on_page) entry_ids.add(id);
+      entries = entries.filter(entry => entry_ids.has(String(entry._id)));
+
+      // Sanitize entries, replace cache with cache_hb if needed
+      //
+      entries = await sanitize_entry(N, entries, locals.params.user_info);
+      locals.res.blog_entries = _.keyBy(entries, '_id');
+
+      // Collect user ids
+      //
+      locals.users = locals.users || [];
+      locals.users = locals.users.concat(_.map(entries, 'user'));
+      locals.users = locals.users.concat(_.map(entries, cache + '.last_user').filter(Boolean));
+
+      locals.res.read_marks = {};
+      for (let id of entry_ids) locals.res.read_marks[id] = read_marks[id];
+    }
   });
 };
