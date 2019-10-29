9fc142e5f6795285c4dd068ffdd90c99767ed2ee
Alex Kocharin <alex@kocharin.ru>
2019-10-29 13:08:58 +0300

    Implement a cut md plugin for blog entries

--- db/migrate/20191029143224_set_markup_permissions.js
+++ db/migrate/20191029143224_set_markup_permissions.js
@@ -6,5 +6,9 @@ module.exports.up = async function (N) {
 
   if (!global_store) throw 'Settings store `global` is not registered.';
 
+  // disable attachments in comments (enabled by default)
   await global_store.set({ blog_comments_markup_attachment: { value: false } });
+
+  // enable cut in blog entries (disabled by default)
+  await global_store.set({ blog_entries_markup_cut: { value: true } });
 };
