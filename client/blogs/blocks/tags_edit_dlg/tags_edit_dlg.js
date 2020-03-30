0e8fcb3282531fe1d28c8914f914de6d3cc80c71
Alex Kocharin <alex@kocharin.ru>
2020-03-30 15:14:21 +0300

    Add tag autocompletion in blogs

--- client/blogs/blocks/tags_edit_dlg/tags_edit_dlg.js
+++ client/blogs/blocks/tags_edit_dlg/tags_edit_dlg.js
@@ -7,11 +7,21 @@ const _ = require('lodash');
 
 let $dialog;
 let result;
+let bloodhound;
+
+
+// Load dependencies
+//
+N.wire.before(module.apiPath, function load_deps() {
+  return N.loader.loadAssets([ 'vendor.typeahead' ]);
+});
 
 
 // Init dialog
 //
 N.wire.on(module.apiPath, function show_dialog(data) {
+  const Bloodhound = require('corejs-typeahead/dist/bloodhound.js');
+
   result = null;
   $dialog = $(N.runtime.render(module.apiPath, _.assign({ apiPath: module.apiPath }, data)));
   $('body').append($dialog);
@@ -19,6 +29,87 @@ N.wire.on(module.apiPath, function show_dialog(data) {
   return new Promise((resolve, reject) => {
     $dialog
       .on('shown.bs.modal', function () {
+        // If bloodhound not initialized - init
+        //
+        if (!bloodhound) {
+          bloodhound = new Bloodhound({
+            remote: {
+              url: 'unused', // bloodhound throws if it's not defined
+              prepare(tag) { return tag; },
+              // Reroute request to rpc
+              transport(req, onSuccess, onError) {
+                N.io.rpc('blogs.tag_lookup', { tag: req.url })
+                  .then(onSuccess)
+                  .catch(onError);
+              }
+            },
+            datumTokenizer: Bloodhound.tokenizers.whitespace,
+            queryTokenizer: Bloodhound.tokenizers.whitespace
+          });
+
+          bloodhound.initialize();
+        }
+
+        let bloodhound_adapter = bloodhound.ttAdapter();
+        let current_tags = '';
+        let current_pos  = 0;
+
+        function extractTag(str, pos) {
+          let m_before = str.slice(0, pos).match(/^(.*,\s*)?([^,]*)$/);
+          let m_after  = str.slice(pos).match(/^([^,]*)(\s*,.*)?$/);
+
+          return {
+            before:  m_before[1] || '',
+            current: (m_before[2] || '') + (m_after[1] + ''),
+            after:   m_after[2] || ''
+          };
+        }
+
+        function beforeReplace() {
+          current_tags = $('.blogs-tag-edit__tags.tt-input').val();
+          current_pos  = $('.blogs-tag-edit__tags.tt-input').prop('selectionStart');
+        }
+
+        function onReplace(event, data) {
+          if (!data) {
+            // reset to original set of tags
+            $('.blogs-tag-edit__tags.tt-input').prop('selectionStart', current_pos);
+            $('.blogs-tag-edit__tags.tt-input').prop('selectionEnd',   current_pos);
+            return;
+          }
+
+          let existing_tags = extractTag(current_tags, current_pos);
+          let new_tags      = extractTag($('.blogs-tag-edit__tags.tt-input').val(),
+                              $('.blogs-tag-edit__tags.tt-input').prop('selectionStart'));
+          let new_val = existing_tags.before + new_tags.current + existing_tags.after;
+
+          if (event.type === 'typeahead:selected') {
+            $('.blogs-tag-edit__tags').typeahead('val', new_val);
+          } else {
+            $('.blogs-tag-edit__tags.tt-input').val(new_val);
+          }
+
+          $('.blogs-tag-edit__tags.tt-input').prop('selectionStart', current_pos);
+          $('.blogs-tag-edit__tags.tt-input').prop('selectionEnd',   current_pos);
+        }
+
+        // Bind typeahead to tag field
+        //
+        $('.blogs-tag-edit__tags').typeahead({ highlight: true }, {
+          source: (query, ...args) => {
+            current_tags = $('.blogs-tag-edit__tags.tt-input').val();
+            current_pos  = $('.blogs-tag-edit__tags.tt-input').prop('selectionStart');
+
+            query = extractTag(current_tags, current_pos).current;
+
+            bloodhound_adapter.call(bloodhound, query, ...args);
+          }
+        })
+          .on('typeahead:beforeselect', beforeReplace)
+          .on('typeahead:beforecursorchange', beforeReplace)
+          .on('typeahead:selected', onReplace)
+          .on('typeahead:cursorchanged', onReplace);
+
         $dialog.find('.btn-secondary').focus();
       })
       .on('hidden.bs.modal', function () {
---
+++ server/blogs/tag_lookup.js
@@ -0,0 +1,43 @@
+// Select list of tags owned by current user
+//
+'use strict';
+
+
+const _ = require('lodash');
+
+
+module.exports = function (N, apiPath) {
+  N.validate(apiPath, {
+    tag: { type: 'string', required: true }
+  });
+
+
+  // Check user permissions
+  //
+  N.wire.before(apiPath, function check_permissions(env) {
+    if (!env.user_info.is_member) throw N.io.NOT_FOUND;
+  });
+
+
+  // Find tags and fill response
+  //
+  N.wire.on(apiPath, async function find_tags(env) {
+    if (env.params.tag.length < 3) {
+      env.res = [];
+      return;
+    }
+
+    let normalized_prefix = N.models.blogs.BlogTag.normalize(env.params.tag);
+
+    let tags = await N.models.blogs.BlogTag.find()
+                         .where('user').equals(env.user_info.user_id)
+                         .where('name_lc').regex(
+                             new RegExp('^' + _.escapeRegExp(normalized_prefix)))
+                         .sort('name_lc')
+                         .limit(10)
+                         .select('-_id name_lc')
+                         .lean(true);
+
+    env.res = _.map(tags, 'name_lc');
+  });
+};
