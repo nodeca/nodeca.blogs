e265f8df30c1f631b8af8f9e0f51197811012968
Alex Kocharin <alex@kocharin.ru>
2021-10-15 17:03:37 +0300

    Disable headings for everything except blogs

--- db/migrate/20191029143224_set_markup_permissions.js
+++ db/migrate/20191029143224_set_markup_permissions.js
@@ -9,6 +9,7 @@ module.exports.up = async function (N) {
   // disable attachments in comments (enabled by default)
   await global_store.set({ blog_comments_markup_attachment: { value: false } });
 
-  // enable cut in blog entries (disabled by default)
+  // enable cut and heading in blog entries (disabled by default)
   await global_store.set({ blog_entries_markup_cut: { value: true } });
+  await global_store.set({ blog_entries_markup_heading: { value: true } });
 };
